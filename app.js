85fdbc0bdd4ab659edbc7340a08566c7e0cdd61e
akanimax <akanimax@gmail.com>
2016-12-09 23:34:40 +0530

    More functionality added

--- app.js
+++ app.js
@@ -3,7 +3,19 @@
 
   angular.module("valueCalculator", [])
 
-  .controller("valueCalculatorController", function ($scope) {
+  .controller("valueCalculatorController", valueCalculatorController)
+
+  .controller("DIController", DIController)
+
+  .controller("bulbController", bulbController);
+
+  valueCalculatorController.$injector = ["$scope"];
+
+  DIController.$injector = ["$scope", "$filter"];
+
+  bulbController.$injector = ["$scope"]
+
+  function valueCalculatorController ($scope) {
     $scope.name = "";
     $scope.value = 0;
 
@@ -20,7 +32,27 @@
 
         return count;
     }
+  }
 
-  });
+  function DIController ($scope, $filter) {
+      $scope.name = "Animesh";
+
+      $scope.upper = function () {
+        var makeUpper = $filter("uppercase");
+        $scope.name = makeUpper($scope.name);
+      };
+  }
+
+  function bulbController ($scope) {
+    $scope.stateOfBulb = "off";
+
+    $scope.changeBulbState = function () {
+      if($scope.stateOfBulb == "on") {
+        $scope.stateOfBulb = "off";
+      } else {
+        $scope.stateOfBulb = "on";
+      }
+    };
+  }
 
 })();
