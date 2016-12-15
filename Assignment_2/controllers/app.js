4bfec3b95e7e272f6a233acf96b958306088c7dc
akanimax <akanimax@gmail.com>
2016-12-15 20:13:12 +0530

    very small change

--- Assignment_2/controllers/app.js
+++ Assignment_2/controllers/app.js
@@ -1,4 +1,6 @@
 (function () {
+  "use strict";
+
   angular.module("ShoppingListCheckOff", [])
     .controller("ToBuyController", ToBuyController)
     .controller("AlreadyBoughtController", AlreadyBoughtController)
